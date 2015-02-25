ec51c21e4065700cb3ca98dcf357ef6cfcc3f83e
Max Gibson <max.gibson93@gmail.com>
2015-02-25 09:30:51 -0800

    latest progress on new html files

---
+++ js/sidenav.js
@@ -0,0 +1,45 @@
+//positioning and interactivity for dynamic sidenav
+$(document).ready(function(){
+	$('#jsparagraph').html("yay jQuery");
+$( "a.link" ).hover(
+	function(){
+		$(this).parent().parent().children(".nav-item-text").css("visibility", "visible");
+	}, function() {
+		$(this).parent().parent().children(".nav-item-text").css("visibility", "hidden");
+	}
+ );
+    sideNavPosition();
+});
+
+/*taken from http://jsfiddle.net/anjzb/ */
+$(window).scroll(sideNavPosition);
+
+function sideNavPosition(){
+    var sOffset = $(".above-side-nav").offset().top;
+    var barheight = $(".above-side-nav").height(); //add 60 to account for margin
+    var y = $(this).scrollTop();
+    
+
+    if (y > sOffset + barheight) {
+        console.log("moving");
+        $("#side-nav").css({
+            'margin-top': '20px',
+        });
+    } else {
+        $("#side-nav").css({
+            'margin-top': ((sOffset+barheight) - y)+ 'px', //310 is height of all top bars above side nav
+        });
+    }
+
+    $('.link').each(function (event) {
+        if (y >= $($(this).attr('href')).offset().top - 100) {
+                // $('.link').not(this).removeClass('active');
+                $('.link').not(this).parent().parent().children(".nav-item-text").css("visibility", "hidden");
+                // $(this).addClass('active');
+                $(this).parent().parent().children(".nav-item-text").css("visibility", "visible");
+            }
+        });
+}
+
+
+
